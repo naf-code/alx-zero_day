ec6e431411cab17060f21a40c3dc6028234d34cb
naf-code <58930877+naf-code@users.noreply.github.com>
2022-03-22 16:18:35 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+
 #include <stdio.h>
 
 /**
@@ -12,7 +12,7 @@ int main(void)
 
     n = 402;
     printf("n=%d\n", n);
-    reset_to_98(&n);
+    //reset_to_98(&n);
     printf("n=%d\n", n);
     return (0);
 }
